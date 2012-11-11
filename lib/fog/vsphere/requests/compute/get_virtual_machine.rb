ec29305ce1963e3dbd2cc269c66ba6f3551f01c1
Nick Huanca <nhuanca@pingidentity.com>
2012-11-11 09:13:28 -0800

    uuid find is first in get_virtual_machine.rb. removed some dead code

--- lib/fog/vsphere/requests/compute/get_virtual_machine.rb
+++ lib/fog/vsphere/requests/compute/get_virtual_machine.rb
@@ -11,26 +11,25 @@ module Fog
         def get_vm_ref(id, datacenter_name = nil)
           # The required id syntax - 'topfolder/subfolder/anotherfolder'
           # or uuid
-          if datacenter_name
-            # The required path syntax - 'topfolder/subfolder/anotherfolder'
-            path_ary = id.split('/')
-            vm_name = path_ary.pop
-            dc = get_raw_datacenter(datacenter_name)
-            dc_root_folder = dc.vmFolder
-            # try to find based on VM name if datacenter is set
-            # Walk the tree resetting the folder pointer as we go
-            folder = get_raw_folder(path_ary.join('/'), datacenter_name)
-            vm = folder.find(vm_name, RbVmomi::VIM::VirtualMachine)
-            raise Fog::Compute::Vsphere::NotFound, "#{id} was not found or returned as a folder" unless vm
-          else
-            vm = case id
-              # UUID based search (needs test)
-              when /[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}/
-                @connection.searchIndex.FindByUuid :uuid => id, :vmSearch => true, :instanceUuid => true, :datacenter => datacenter_name
-              else
-                # Don't believe this works needs test
-                raw_datacenters.map { |d| d.find_vm(id) }.compact.first
+          if datacenter
+            if id =~ /[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}/
+              vm = @connection.searchIndex.FindByUuid :uuid => id, :vmSearch => true, :instanceUuid => true, :datacenter => datacenter_name
+            else
+              # The required path syntax - 
+              # 'topfolder/subfolder/anotherfolder/vmname'
+              # split the path up into array, pop off the vm name
+              # join back to string path for folder find.
+              path_ary = id.split('/')
+              vm_name = path_ary.pop
+              path = path_ary.join('/')
+              # try to find based on VM name if datacenter is set
+              folder = get_raw_folder(path, datacenter_name)
+              vm = folder.find(vm_name, RbVmomi::VIM::VirtualMachine)
+              raise Fog::Compute::Vsphere::NotFound, "#{id} was not found or returned as a folder" unless vm
             end
+          else
+            # Don't believe this works needs test
+            raw_datacenters.map { |d| d.find_vm(id) }.compact.first
           end
           vm ? vm : raise(Fog::Compute::Vsphere::NotFound, "#{id} was not found")
         end
